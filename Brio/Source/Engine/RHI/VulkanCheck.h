550bd08c3ff3cf8537fe902eb553108a4d3fc22c
Simon <code.cymon@gmail.com>
2026-03-30 20:40:11 +0200

    expanded VK_CHECK failure message

--- Brio/Source/Engine/RHI/VulkanCheck.h
+++ Brio/Source/Engine/RHI/VulkanCheck.h
@@ -4,5 +4,4 @@
 
 #include <vulkan/vk_enum_string_helper.h>
 
-#define VK_CHECK(result) \
-    ASSERT(result == VK_SUCCESS, string_VkResult(result));
+#define VK_CHECK(result) ASSERT(result == VK_SUCCESS, "Vulkan Action failed: {}", string_VkResult(result));
