a0e6f9ddc5aa8ca313f733509b641fc8fe2f41f1
Simon <code.cymon@gmail.com>
2026-04-03 01:53:44 +0200

    fixed build.sh not reporting correct error code - symlink was always returning OK as last op -> now chained to other ops

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 # build.sh
 CONFIG=${1:-Debug}
 cmake --preset $(echo $CONFIG | tr '[:upper:]' '[:lower:]') && \
-cmake --build --preset $(echo $CONFIG | tr '[:upper:]' '[:lower:]')
+cmake --build --preset $(echo $CONFIG | tr '[:upper:]' '[:lower:]') && \
 ln -sf Intermediate/$CONFIG/compile_commands.json compile_commands.json
