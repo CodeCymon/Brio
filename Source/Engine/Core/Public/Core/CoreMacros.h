1d8fdc9b354b31e0074453833608d337bbe9b492
Simon <code.cymon@gmail.com>
2026-08-23 14:14:27 +0200

    Core: enum bitwise operator macro

--- Source/Engine/Core/Public/Core/CoreMacros.h
+++ Source/Engine/Core/Public/Core/CoreMacros.h
@@ -1,6 +1,7 @@
 // Copyright (c) Simon Kirsch 2026.
 
 #pragma once
+#include <type_traits>
 
 #define NON_COPYABLE(ClassName) \
     ClassName(ClassName const&) = delete; \
@@ -10,3 +11,29 @@
     ClassName(ClassName&&) = delete; \
     ClassName& operator=(ClassName&&) = delete;
 
+#define ENABLE_ENUM_BITWISE_OPERATORS(Enum)                              \
+    inline constexpr Enum operator|(Enum lhs, Enum rhs) {                \
+        using U = std::underlying_type_t<Enum>;                          \
+        return static_cast<Enum>(static_cast<U>(lhs) | static_cast<U>(rhs)); \
+    }                                                                    \
+    inline constexpr Enum operator&(Enum lhs, Enum rhs) {                \
+        using U = std::underlying_type_t<Enum>;                          \
+        return static_cast<Enum>(static_cast<U>(lhs) & static_cast<U>(rhs)); \
+    }                                                                    \
+    inline constexpr Enum operator^(Enum lhs, Enum rhs) {                \
+        using U = std::underlying_type_t<Enum>;                          \
+        return static_cast<Enum>(static_cast<U>(lhs) ^ static_cast<U>(rhs)); \
+    }                                                                    \
+    inline constexpr Enum operator~(Enum value) {                        \
+        using U = std::underlying_type_t<Enum>;                          \
+        return static_cast<Enum>(~static_cast<U>(value));                \
+    }                                                                    \
+    inline constexpr Enum& operator|=(Enum& lhs, Enum rhs) {             \
+        return lhs = lhs | rhs;                                          \
+    }                                                                    \
+    inline constexpr Enum& operator&=(Enum& lhs, Enum rhs) {             \
+        return lhs = lhs & rhs;                                          \
+    }                                                                    \
+    inline constexpr Enum& operator^=(Enum& lhs, Enum rhs) {             \
+        return lhs = lhs ^ rhs;                                          \
+    }
