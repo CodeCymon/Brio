fb71f1b36109e49668d29cb65c5b6e0784336c85
Simon <code.cymon@gmail.com>
2026-09-03 00:07:17 +0200

    VulkanRHI: removed "barrier.h/.cpp" -> moved imagestate conversion into translation utils instead

--- Source/Engine/VulkanRHI/Private/TranslationUtils/TextureTranslations.cpp
+++ Source/Engine/VulkanRHI/Private/TranslationUtils/TextureTranslations.cpp
@@ -90,6 +90,38 @@ VkSampleCountFlagBits ImageTranslation::ToVulkanSampleCount(u32 sampleCount) {
     return VK_SAMPLE_COUNT_FLAG_BITS_MAX_ENUM;
 }
 
+VulkanImageStateInfo ImageTranslation::ToVulkanImageState(RHIResourceState state) {
+    switch (state) {
+        case RHIResourceState::Undefined:
+            return { VK_IMAGE_LAYOUT_UNDEFINED, VK_PIPELINE_STAGE_2_NONE, VK_ACCESS_2_NONE };
+        case RHIResourceState::TransferSrc:
+            return { VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL, VK_PIPELINE_STAGE_2_TRANSFER_BIT, VK_ACCESS_2_TRANSFER_READ_BIT };
+        case RHIResourceState::TransferDst:
+            return { VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_PIPELINE_STAGE_2_TRANSFER_BIT, VK_ACCESS_2_TRANSFER_WRITE_BIT };
+        case RHIResourceState::ColorAttachment:
+            return { VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL, VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT,
+                      VK_ACCESS_2_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_2_COLOR_ATTACHMENT_WRITE_BIT };
+        case RHIResourceState::DepthStencilAttachment:
+            return { VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+                      VK_PIPELINE_STAGE_2_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_2_LATE_FRAGMENT_TESTS_BIT,
+                      VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT };
+        case RHIResourceState::DepthStencilReadOnly:
+            return { VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+                      VK_PIPELINE_STAGE_2_EARLY_FRAGMENT_TESTS_BIT | VK_PIPELINE_STAGE_2_LATE_FRAGMENT_TESTS_BIT,
+                      VK_ACCESS_2_DEPTH_STENCIL_ATTACHMENT_READ_BIT };
+        case RHIResourceState::ShaderReadOnly:
+            // TODO: specialise pipeline stage with RDG info
+            return { VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT, VK_ACCESS_2_SHADER_READ_BIT };
+        case RHIResourceState::General:
+            return { VK_IMAGE_LAYOUT_GENERAL, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
+                      VK_ACCESS_2_SHADER_READ_BIT | VK_ACCESS_2_SHADER_WRITE_BIT };
+        case RHIResourceState::Present:
+            return { VK_IMAGE_LAYOUT_PRESENT_SRC_KHR, VK_PIPELINE_STAGE_2_NONE, VK_ACCESS_2_NONE };
+    }
+    ASSERTM(false, "Unhandled ResourceState");
+    return {};
+}
+
 VkImageCreateInfo ImageTranslation::CreateInfoFromTextureDesc(RHITextureDesc const &desc) {
     VkImageCreateInfo createInfo = {
         .sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO,
--- Source/Engine/VulkanRHI/Private/TranslationUtils/TextureTranslations.h
+++ Source/Engine/VulkanRHI/Private/TranslationUtils/TextureTranslations.h
@@ -5,12 +5,21 @@
 
 #include "RHIResources.h"
 
+struct VulkanImageStateInfo {
+    VkImageLayout layout;
+    VkPipelineStageFlags2 stage;
+    VkAccessFlags2 access;
+};
+
+
 namespace ImageTranslation {
     VkImageType ToVulkanDimensionType(TextureDimension dimension);
     VkImageUsageFlags ToVulkanUsageFlags(TextureUsage usage);
     VkFormat ToVulkanFormat(PixelFormat format);
     VkImageAspectFlags ToVulkanImageAspectFlags(PixelFormat format);
     VkSampleCountFlagBits ToVulkanSampleCount(u32 sampleCount);
+
+    VulkanImageStateInfo ToVulkanImageState(RHIResourceState state);
 
     VkImageCreateInfo CreateInfoFromTextureDesc(RHITextureDesc const& desc);
 };
